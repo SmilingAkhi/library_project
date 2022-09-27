70d413f1777fbfb5f877046a5f98c651139d1118
Abdulrahman <bhabdulrahaman@gmail.com>
2022-09-27 20:19:26 +0100

    added function to fetch user inputs

--- library.js
+++ library.js
@@ -1,11 +1,15 @@
 "use script";
 const bookContainer = document.querySelector(".bookContainer");
+const displayFormBtn = document.querySelector(".add-btn");
+const formModal = document.querySelector(".modalContainer");
+const submit = document.querySelector(".submit");
 
+//array to store the book
 let myLibrary = [];
 console.log(myLibrary);
 
+//the book constructor
 function Book(title, author, pages, haveRead) {
-  //the constructor
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -14,10 +18,37 @@ function Book(title, author, pages, haveRead) {
     return ` The ${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead} `;
   };
 }
+
+//using event listener to add a function to getBookInfo
+displayFormBtn.addEventListener("click", displayForm);
+
+//function to display form
+function displayForm() {
+  formModal.classList.remove("hidden");
+}
+
+//using event listener to add function to submit button
+submit.addEventListener("click", getFormInfo);
+
+//function to get form info
+function getFormInfo() {
+  //fetching the form info
+  const titleInputed = document.querySelector(".title").value;
+  const authorInputed = document.querySelector(".author").value;
+  const pagesInputed = document.querySelector(".pages").value;
+  const readValueInputed = document.querySelector(".readCheck").value;
+
+  console.log(titleInputed, authorInputed, pagesInputed, readValueInputed);
+
+  //removing the form after clicking submit
+  //   formModal.classList.add("hidden");
+}
+
 const Spitter = new Book("spitter", "bhadmus", 200, "yes");
 const crammer = new Book("crammer", "kelvin", 560, "yes");
 const fisher = new Book("fisher", "smith", 88, "yes");
 
+//function to push the created books into array
 function addBookToLibrary(book) {
   // do stuff here
   myLibrary.push(book);
@@ -27,24 +58,26 @@ addBookToLibrary(Spitter);
 addBookToLibrary(crammer);
 addBookToLibrary(fisher);
 
+//function to loop through the library and display it on the screen
 function loopingMyLibrary() {
+  //getting each book the array
   for (const eachBook of myLibrary) {
     console.log(eachBook);
-
+    //conatiner for each book
     const eachBookContainer = document.createElement("div");
 
     eachBookContainer.classList.add("book");
     console.log(eachBookContainer);
 
     const eachBookTitle = document.createElement("p");
-    const eachBookaurthor = document.createElement("p");
+    const eachBookauthor = document.createElement("p");
     const eachBookPages = document.createElement("p");
 
     eachBookTitle.textContent = ` TITLE: ${eachBook.title}`;
     eachBookContainer.appendChild(eachBookTitle);
 
-    eachBookaurthor.textContent = `AURTHOR: ${eachBook.author}`;
-    eachBookContainer.appendChild(eachBookaurthor);
+    eachBookauthor.textContent = `AUTHOR: ${eachBook.author}`;
+    eachBookContainer.appendChild(eachBookauthor);
 
     eachBookPages.textContent = `PAGES:${eachBook.pages}`;
     eachBookContainer.appendChild(eachBookPages);
