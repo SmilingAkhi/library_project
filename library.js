13ab9d24b53c02a6b4dde1f09c09463b421fb1a9
Abdulrahman <bhabdulrahaman@gmail.com>
2022-10-01 00:48:00 +0100

    cretaed a book instance, pushed it to the object arraym read it and displayed it on the screen

--- library.js
+++ library.js
@@ -1,25 +1,11 @@
 "use script";
+//our global variables
 const bookContainer = document.querySelector(".bookContainer");
 const displayFormBtn = document.querySelector(".add-btn");
 const formModal = document.querySelector(".modalContainer");
 const submit = document.querySelector(".submit");
 const inputField = document.querySelectorAll(".input-field");
 const closeForm = document.querySelector(".closeForm");
-
-//array to store the book
-let myLibrary = [];
-console.log(myLibrary);
-
-//the book constructor
-function Book(title, author, pages, haveRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.haveRead = haveRead;
-  this.info = function () {
-    return ` The ${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead} `;
-  };
-}
 
 //displaying form
 displayFormBtn.addEventListener("click", () => {
@@ -71,38 +57,61 @@ function getFormInfo() {
     formModal.style.display = "none";
   }
 
+  //array to store the book
+  let myLibrary = [];
+  console.log(myLibrary);
+
+  //the book constructor
+  function Book(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+    this.info = function () {
+      return ` The ${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead} `;
+    };
+  }
+  //create abook instance
+  const newBook = new Book(
+    titleInputed,
+    authorInputed,
+    pagesInputed,
+    readValueInputed
+  );
   //function to push the created books into array
   function addBookToLibrary(book) {
     myLibrary.push(book);
   }
   addBookToLibrary(newBook);
-}
 
-//function to loop through the library and display it on the screen
-function loopingMyLibrary() {
-  //getting each book the array
-  for (const eachBook of myLibrary) {
-    console.log(eachBook);
-    //conatiner for each book
-    const eachBookContainer = document.createElement("div");
+  //function to loop through the library and display it on the screen
+  function loopingMyLibrary() {
+    //getting each book the array
+    for (const eachBook of myLibrary) {
+      console.log(eachBook);
+      //conatiner for each book
+      const eachBookContainer = document.createElement("div");
 
-    eachBookContainer.classList.add("book");
-    console.log(eachBookContainer);
+      eachBookContainer.classList.add("book");
+      console.log(eachBookContainer);
 
-    const eachBookTitle = document.createElement("p");
-    const eachBookauthor = document.createElement("p");
-    const eachBookPages = document.createElement("p");
+      const eachBookTitle = document.createElement("p");
+      const eachBookauthor = document.createElement("p");
+      const eachBookPages = document.createElement("p");
 
-    eachBookTitle.textContent = ` TITLE: ${eachBook.title}`;
-    eachBookContainer.appendChild(eachBookTitle);
+      eachBookTitle.textContent = ` TITLE: ${eachBook.title}`;
+      eachBookContainer.appendChild(eachBookTitle);
 
-    eachBookauthor.textContent = `AUTHOR: ${eachBook.author}`;
-    eachBookContainer.appendChild(eachBookauthor);
+      eachBookauthor.textContent = `AUTHOR: ${eachBook.author}`;
+      eachBookContainer.appendChild(eachBookauthor);
 
-    eachBookPages.textContent = `PAGES:${eachBook.pages}`;
-    eachBookContainer.appendChild(eachBookPages);
+      eachBookPages.textContent = `PAGES:${eachBook.pages}`;
+      eachBookContainer.appendChild(eachBookPages);
 
-    bookContainer.appendChild(eachBookContainer);
+      bookContainer.appendChild(eachBookContainer);
+    }
   }
+  loopingMyLibrary();
+  ` 
+  `;
 }
-loopingMyLibrary();
